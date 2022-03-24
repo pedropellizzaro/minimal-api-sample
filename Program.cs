bd11ba183cae0217abf0db8237303890a807344f
Pedro Pelizzaro <pedropellizzaro@hotmail.com>
2022-03-24 08:46:05 -0300

    add jwt authentication with netdevpack

--- Program.cs
+++ Program.cs
@@ -1,16 +1,30 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using MinimalApiSample.Data;
 using MinimalApiSample.Models;
 using MiniValidation;
+using NetDevPack.Identity;
+using NetDevPack.Identity.Jwt;
+using NetDevPack.Identity.Model;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
 builder.Services.AddDbContext<ApplicationContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddIdentityEntityFrameworkContextConfiguration(options =>
+    options.UseSqlServer(connectionString,
+    b => b.MigrationsAssembly(typeof(ApplicationContext).Assembly.FullName)));
+
+builder.Services.AddIdentityConfiguration();
+builder.Services.AddJwtConfiguration(builder.Configuration, "AppSettings");
 
 var app = builder.Build();
 
@@ -19,8 +33,86 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseAuthConfiguration();
 app.UseHttpsRedirection();
+
+app.MapPost("/register", async (
+    SignInManager<IdentityUser> signInManager,
+    UserManager<IdentityUser> userManager,
+    IOptions<AppJwtSettings> jwtSettings,
+    RegisterUser registerUser) =>
+    {
+        if (registerUser is null)
+            return Results.BadRequest("Usuário não informado.");
+
+        if (!MiniValidator.TryValidate(registerUser, out var errors))
+            return Results.ValidationProblem(errors);
+
+        var user = new IdentityUser
+        {
+            UserName = registerUser.Email,
+            Email = registerUser.Email,
+            EmailConfirmed = true
+        };
+
+        var result = await userManager.CreateAsync(user, registerUser.Password);
+
+        if (!result.Succeeded)
+            return Results.BadRequest(result.Errors);
+
+        var jwt = new JwtBuilder()
+            .WithUserManager(userManager)
+            .WithJwtSettings(jwtSettings.Value)
+            .WithEmail(user.Email)
+            .WithJwtClaims()
+            .WithUserClaims()
+            .WithUserRoles()
+            .BuildUserResponse();
+
+        return Results.Ok(jwt);
+    })
+    .ProducesValidationProblem()
+    .Produces(StatusCodes.Status200OK)
+    .Produces(StatusCodes.Status400BadRequest)
+    .WithName("RegisterUser")
+    .WithTags("User");
+
+app.MapPost("login", async (
+    SignInManager<IdentityUser> signInManager,
+    UserManager<IdentityUser> userManager,
+    IOptions<AppJwtSettings> jwtSettings,
+    LoginUser user) =>
+{
+    if (user is null)
+        return Results.BadRequest("Usuário não informado.");
+
+    if (!MiniValidator.TryValidate(user, out var errors))
+        return Results.ValidationProblem(errors);
+
+    var result = await signInManager.PasswordSignInAsync(user.Email, user.Password, true, true);
+
+    if (result.IsLockedOut)
+        return Results.BadRequest("Usuário bloqueado.");
+
+    if (!result.Succeeded)
+        return Results.BadRequest("Usuário ou senha inválidos.");
+
+    var jwt = new JwtBuilder()
+        .WithUserManager(userManager)
+        .WithJwtSettings(jwtSettings.Value)
+        .WithEmail(user.Email)
+        .WithJwtClaims()
+        .WithUserClaims()
+        .WithUserRoles()
+        .BuildUserResponse();
+
+    return Results.Ok(jwt);
+})
+    .ProducesValidationProblem()
+    .Produces(StatusCodes.Status400BadRequest)
+    .Produces(StatusCodes.Status200OK)
+    .WithName("Login")
+    .WithTags("User");
 
 app.MapGet("/supplier", async (ApplicationContext context) =>
     await context.Suppliers.ToListAsync())
@@ -37,23 +129,24 @@ app.MapGet("supplier/{id}", async (Guid id, ApplicationContext context) =>
     .WithName("GetSupplier")
     .WithTags("Supplier");
 
-app.MapPost("/supplier", async (ApplicationContext context, [FromBody] Supplier model) =>
-{
-    if (!MiniValidator.TryValidate(model, out var errors))
-        return Results.ValidationProblem(errors);
+app.MapPost("/supplier", async (ApplicationContext context, 
+    [FromBody] Supplier model) =>
+    {
+        if (!MiniValidator.TryValidate(model, out var errors))
+            return Results.ValidationProblem(errors);
 
-    context.Suppliers.Add(model);
-    var saved = await context.SaveChangesAsync() > 0;
+        context.Suppliers.Add(model);
+        var saved = await context.SaveChangesAsync() > 0;
 
-    return saved
-        ? Results.CreatedAtRoute("GetSupplier", new { model.Id }, model)
-        : Results.BadRequest("There was an error inserting supplier.");
-})
-.ProducesValidationProblem()
-.Produces<Supplier>(StatusCodes.Status201Created)
-.Produces(StatusCodes.Status400BadRequest)
-.WithName("InsertSupplier")
-.WithTags("Supplier");
+        return saved
+            ? Results.CreatedAtRoute("GetSupplier", new { model.Id }, model)
+            : Results.BadRequest("There was an error inserting supplier.");
+    })
+    .ProducesValidationProblem()
+    .Produces<Supplier>(StatusCodes.Status201Created)
+    .Produces(StatusCodes.Status400BadRequest)
+    .WithName("InsertSupplier")
+    .WithTags("Supplier");
 
 app.MapPut("supplier/{id}", async (
     ApplicationContext context,
